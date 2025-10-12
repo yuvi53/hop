68f09c0f60eca455b630920be23db7d2695c70b1
yuvi <exghostuser@gmail.com>
2025-10-12 21:40:44 +0530

    add public fuction match_path and also change fuction calling apis

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,11 @@
 use std::error::Error;
 use clap::{Arg, Command, ArgAction};
-use hop::{add_path, search_path, get_data};
+use hop::{
+    add_path,
+    search_for_path,
+    get_data,
+    match_path
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = Command::new("whatever")
@@ -13,7 +18,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         .get_matches();
     let data = get_data()?;
     if let Some(dir) = matches.get_one::<String>("dir") {
-        println!("{}", search_path(&data, dir.clone())?);
+        let vec_of_paths = search_for_path(&data, dir.clone());
+        println!("{}", match_path(&vec_of_paths)?);
     }
     if let Some(path) = matches.get_one::<String>("add") {
         add_path(&data, path.clone(), None)?;
